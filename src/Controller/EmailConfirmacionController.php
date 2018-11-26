d75491d81bb9cf93983d0bbc4d1ce53bd00852bb
marcoss94 <marcossalvarez94@gmail.com>
2018-11-26 13:48:14 +0100

    correo botones de caroussel y form de carro

--- src/Controller/EmailConfirmacionController.php
+++ src/Controller/EmailConfirmacionController.php
@@ -8,11 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class EmailConfirmacionController extends Controller
 {
     /**
-     * @Route("/email/confirmacion", name="email_confirmacion")
-     */
+ * @Route("/email/confirmacion", name="email_confirmacion")
+ */
     public function index()
     {
         return $this->render('email_confirmacion/index.html.twig', [
+            'controller_name' => 'EmailConfirmacionController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/email/pagos", name="email_pagos")
+     */
+    public function email_pagos()
+    {
+        return $this->render('email_confirmacion/email_pagos.html.twig', [
+            'controller_name' => 'EmailConfirmacionController',
+        ]);
+    }
+
+    /**
+     * @Route("/email/nopagos", name="email_noPagos")
+     */
+    public function email_nopagos()
+    {
+        return $this->render('email_confirmacion/email_noPagos.html.twig', [
             'controller_name' => 'EmailConfirmacionController',
         ]);
     }
