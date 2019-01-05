07a4d392294b2a32d305b235d4632e6dce5b6639
armando <armando25723@gmail.com>
2019-01-05 18:14:10 +0100

    arreglando las listas

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -36,7 +36,7 @@ class TransactionController extends Controller
         $payment = $storage->create();
         $payment->setNumber(uniqid());
         $payment->setCurrencyCode('EUR');
-        $payment->setTotalAmount($amount); // 1.23 EUR
+        $payment->setTotalAmount((float)$amount); // 1.23 EUR
         $payment->setDescription('A description');
         $payment->setClientId('8JCDJUTNEV6P2');
         $payment->setClientEmail('cubagps@yahoo.com');
